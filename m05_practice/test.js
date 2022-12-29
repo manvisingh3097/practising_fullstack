c6d043c681ecb3e2b9dfbfd1d1ffa293dbf56032
Manvi singh <manvisingh@Manvis-MacBook-Air.local>
2022-12-29 09:09:29 +0530

    module 5 practice.... primitve datatypes

---
+++ m05_practice/test.js
@@ -0,0 +1,42 @@
+// let firstname="manvi";
+// let student=true;
+// let rollno = 24
+
+// console.log(firstname)
+// console.log(student)
+// console.log(rollno)
+
+// console.log("this is my first check")
+
+// let mathValue = Math.E + Math.PI + Math.sqrt(2);
+// alert(Math.round(mathValue))
+
+// let name=window.prompt("enter your name");
+console.log (Date())
+
+let viewscounter=0;
+let likescounter=0;
+let dislikescounter=0;
+viewscounter= viewscounter+1;
+likescounter = likescounter+1;
+dislikescounter=dislikescounter+1;
+
+console.log(viewscounter)
+console.log(likescounter)
+console.log(dislikescounter)
+
+//unary increment operator
+viewsCounter++;
+viewsCounter++
+likesCounter++
+disLikesCounter++
+console.log(viewsCounter)
+console.log(likesCounter)
+consoleloaldielikosCounterl
+
+
+//unary decrement operator
+viewscounter--;
+likescounter--;
+
+
