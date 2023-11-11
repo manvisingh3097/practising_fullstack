3432bf747a2456b94b1097165dcafd0a9e1c3c68
manvisingh3097 <singh.thakurmanvi@gmail.com>
2023-11-11 21:58:55 +0530

    working on backend

--- Midtermexam/shoplanebackend/shoplaneapp/models.py
+++ Midtermexam/shoplanebackend/shoplaneapp/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
 # Create your models here.
+
+class category(models.Model):
+    category_id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=200)
+    
+    
+class Product(models.Model):
+    category_id = models.IntegerField()
+    name = models.CharField(max_length=200)
+    price = models.FloatField(max_length=200)
+    Description = models.TextField(null=True , blank=True,default=None)
+    category = models.CharField()
+    image = models.URLField(null=True , blank=True)
+    rating = models.IntegerField()   
+    created_at = models.DateTimeField(auto_now_add=True)
--- Midtermexam/shoplanebackend/shoplanebackend/settings.py
+++ Midtermexam/shoplanebackend/shoplanebackend/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     "shoplaneapp",
+    "rest_framework"
 ]
 
 MIDDLEWARE = [
